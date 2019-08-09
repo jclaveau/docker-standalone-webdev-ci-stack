83d6f2a7966efbe263db446e0e456090a064d954
Jean Claveau <jean.claveau@gmail.com>
2019-08-09 10:37:37 +0200

    Working LAMP (MariaDB, PHP 5.6/7.X, PMA) Fix #4

---
+++ examples/docker-example-lamp/www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Lamp Example</title>
+  </head>
+  <body>
+    <h1>Linux / Apache / MariaDB / PHP multiversion / <a href="http://pma.example-lamp.localhost/">PhpMyAdmin</a></h1>
+    <img src="lamp.jpg" alt=";)"/>
+    <div style="overflow-y: auto; ">
+        <?= phpinfo() ?>
+    </div>
+  </body>
+</html>
